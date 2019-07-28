9966124bcadb62687dabf920b8262caabf7951ef
Welson <welsonlimawlsn@gmail.com>
2019-07-28 10:52:29 -0300

    Correção do erro String

--- src/main/java/la/foton/treinamento/desafio/autorizador/log/entity/Log.java
+++ src/main/java/la/foton/treinamento/desafio/autorizador/log/entity/Log.java
@@ -11,105 +11,96 @@
  */
 package la.foton.treinamento.desafio.autorizador.log.entity;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
-
 import la.foton.treinamento.desafio.autorizador.transacao.converter.CanalDeAtendimentoConverter;
 import la.foton.treinamento.desafio.autorizador.transacao.converter.TipoDaTransacaoConverter;
 import la.foton.treinamento.desafio.autorizador.transacao.entity.CanalDeAtendimento;
 import la.foton.treinamento.desafio.autorizador.transacao.entity.TipoDaTransacao;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "LOG")
 public class Log {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "LOGNSU")
-	private Long nsu;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LOGNSU")
+    private Long nsu;
 
-	@Column(name = "LOGDATHOR")
-	private LocalDateTime dataHora;
+    @Column(name = "LOGDATHOR")
+    private LocalDateTime dataHora;
 
-	@Column(name = "LOGDATREF")
-	private LocalDate dataRefencia;
+    @Column(name = "LOGDATREF")
+    private LocalDate dataRefencia;
 
-	@Column(name = "PTACOD")
-	private Integer agencia;
+    @Column(name = "PTACOD")
+    private Integer agencia;
 
-	@Column(name = "TTRCOD")
-	@Convert(converter = TipoDaTransacaoConverter.class)
-	private TipoDaTransacao tipoDaTransacao;
+    @Column(name = "TTRCOD")
+    @Convert(converter = TipoDaTransacaoConverter.class)
+    private TipoDaTransacao tipoDaTransacao;
 
-	@Column(name = "CNLCOD")
-	@Convert(converter = CanalDeAtendimentoConverter.class)
-	private CanalDeAtendimento canal;
+    @Column(name = "CNLCOD")
+    @Convert(converter = CanalDeAtendimentoConverter.class)
+    private CanalDeAtendimento canal;
 
+    @Lob
+    @Column(name = "LOGPRT")
+    private String particao;
 
-	@Column(name = "LOGPRT")
-	private String particao;
+    @PrePersist
+    public void prePersist() {
+        this.dataHora = LocalDateTime.now();
+    }
 
-	@PrePersist
-	public void prePersist() {
-		this.dataHora = LocalDateTime.now();
-	}
+    public Long getNsu() {
+        return nsu;
+    }
 
-	public Long getNsu() {
-		return nsu;
-	}
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
 
-	public LocalDateTime getDataHora() {
-		return dataHora;
-	}
+    public LocalDate getDataRefencia() {
+        return dataRefencia;
+    }
 
-	public LocalDate getDataRefencia() {
-		return dataRefencia;
-	}
+    public void setDataRefencia(LocalDate dataRefencia) {
+        this.dataRefencia = dataRefencia;
+    }
 
-	public void setDataRefencia(LocalDate dataRefencia) {
-		this.dataRefencia = dataRefencia;
-	}
+    public Integer getAgencia() {
+        return agencia;
+    }
 
-	public Integer getAgencia() {
-		return agencia;
-	}
+    public void setAgencia(Integer agencia) {
+        this.agencia = agencia;
+    }
 
-	public void setAgencia(Integer agencia) {
-		this.agencia = agencia;
-	}
+    public TipoDaTransacao getTipoDaTransacao() {
+        return tipoDaTransacao;
+    }
 
-	public TipoDaTransacao getTipoDaTransacao() {
-		return tipoDaTransacao;
-	}
+    public void setTipoDaTransacao(TipoDaTransacao tipoDaTransacao) {
+        this.tipoDaTransacao = tipoDaTransacao;
+    }
 
-	public void setTipoDaTransacao(TipoDaTransacao tipoDaTransacao) {
-		this.tipoDaTransacao = tipoDaTransacao;
-	}
+    public CanalDeAtendimento getCanal() {
+        return canal;
+    }
 
-	public CanalDeAtendimento getCanal() {
-		return canal;
-	}
+    public void setCanal(CanalDeAtendimento canal) {
+        this.canal = canal;
+    }
 
-	public void setCanal(CanalDeAtendimento canal) {
-		this.canal = canal;
-	}
+    public String getParticao() {
+        return particao;
+    }
 
-	public String getParticao() {
-		return particao;
-	}
-
-	public void setParticao(String particao) {
-		this.particao = particao;
-	}
+    public void setParticao(String particao) {
+        this.particao = particao;
+    }
 
 }
